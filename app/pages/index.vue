bc0485febd0d737c0424e79be47cdf04c8d01503
raprap-ui <congcalma@gmail.com>
2026-02-03 10:23:49 +0800

    added layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
-    <v-btn>submit</v-btn>
+    <v-row>
+      <v-col cols="12" md="3"> <v-card height="60" color="blue">
+      <v-card-text>BLUE</v-card-text> 
+      </v-card></v-col>
+
+      <v-col cols="12" md="3"> <v-card height="60" color="red">    </v-card></v-col>
+      <v-col cols="12" md="3"> <v-card height="60" color="green">  </v-card></v-col>
+      <v-col cols="12" md="3"> <v-card height="60" color="yellow"> </v-card></v-col>
+    </v-row>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { VCard } from 'vuetify/components';
+
+
 
 </script>
 
